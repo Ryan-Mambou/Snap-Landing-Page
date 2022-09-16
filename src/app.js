4ac01fa3d4e7f7e7fe09978593c226863b8815de
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-09-16 09:27:42 +0100

    accordion fix

--- src/app.js
+++ src/app.js
@@ -5,14 +5,20 @@ const featureContent = document.querySelector('#featureContent');
 const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 const closeIcon = document.querySelector('#closeIcon');
+const dropdowns = document.querySelectorAll('#dropdown');
 
 function dropdown(element){
+    console.log(element.childNodes)
+    let caret = element.firstElementChild
+    console.log(caret)
     if (element.classList.contains('block')){
         element.classList.remove('block')
+        caret.classList.add('transform', 'rotate-180')
         element.classList.add('hidden')
     }
     else {
         element.classList.remove('hidden')
+        caret.classList.remove('transform', 'rotate-180')
         element.classList.add('block')
     }
 }
@@ -26,10 +32,26 @@ feature.addEventListener('click', () => {
 })
 
 hamburger.addEventListener('click', () => {
-    console.log('click')
     menu.classList.add('translate-x-0')
 })
 
 closeIcon.addEventListener('click', () => {
     menu.classList.remove('translate-x-0');
+})
+
+dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('click', () => {
+        dropdown.classList.toggle('active');
+        console.log(dropdown.firstElementChild)
+        const caret = dropdown.firstElementChild;
+        const panel = dropdown.nextElementSibling;
+        if(dropdown.classList.contains('active')){
+            panel.classList.add('max-h-28')
+            caret.classList.add('transform', 'rotate-180')
+        }
+        else {
+            panel.classList.remove('max-h-28')
+            caret.classList.remove('transform', 'rotate-180')
+        }
+    })
 })
